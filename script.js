43e50fe1e986e3679e338836a177f029247c645e
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-04-27 18:43:05 -0700

    commit 1

---
+++ script.js
@@ -0,0 +1,122 @@
+var nameA = document.querySelector(".name");
+console.log(nameA);
+
+function change(){
+    if (nameA.innerHTML === "Kevin Mosca") {
+        nameA.innerHTML = "Jane Doe";
+    } else {
+        nameA.innerHTML = "Kevin Mosca";
+      }
+}
+
+function remove(){
+    var x = document.getElementById("no");
+    var y = document.getElementById("todd");
+    var z = document.getElementById("check");
+    var n = document.getElementById("txt");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "flex";
+  } else {
+    y.style.display = "none";
+  }
+  if (z.style.display === "none") {
+    z.style.display = "flex";
+  } else {
+    z.style.display = "none";
+  }
+  if (n.innerHTML === " Todd E") {
+    n.innerHTML = " ";
+  } else {
+    n.innerHTML = " Todd E";
+  }
+    
+}
+
+function removeB(){
+    var x = document.getElementById("noB");
+    var y = document.getElementById("Phil");
+    var z = document.getElementById("checkB");
+    var n = document.getElementById("txtB");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "flex";
+  } else {
+    y.style.display = "none";
+  }
+  if (z.style.display === "none") {
+    z.style.display = "flex";
+  } else {
+    z.style.display = "none";
+  }
+  if (n.innerHTML === "Phil K") {
+    n.innerHTML = " ";
+  } else {
+    n.innerHTML = "Phil K";
+    
+ }
+    
+}
+function check(){
+    var x = document.getElementById("no");
+    var y = document.getElementById("todd");
+    var z = document.getElementById("check");
+    var n = document.getElementById("txt");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "flex";
+  } else {
+    y.style.display = "none";
+  }
+  if (z.style.display === "none") {
+    z.style.display = "flex";
+  } else {
+    z.style.display = "none";
+  }
+  if (n.innerHTML === " Todd E") {
+    n.innerHTML = " ";
+  } else {
+    n.innerHTML = " Todd E";
+  }
+    
+}
+function checkB(){
+    var x = document.getElementById("noB");
+    var y = document.getElementById("Phil");
+    var z = document.getElementById("checkB");
+    var n = document.getElementById("txtB");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "flex";
+  } else {
+    y.style.display = "none";
+  }
+  if (z.style.display === "none") {
+    z.style.display = "flex";
+  } else {
+    z.style.display = "none";
+  }
+  if (n.innerHTML === "Phil K") {
+    n.innerHTML = " ";
+  } else {
+    n.innerHTML = "Phil K";
+    
+ }
+    
+}
